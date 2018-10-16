b9209c0e420f91a43504d6ae5569d28e73adbfa1
Jeff Morice <jmorice.anim@gmail.com>
2018-10-15 20:26:43 -0500

    created blog-entry.html to check single post entries and refactored main.py to use it

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template, redirect
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG']=True
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog-TP:password@localhost:8889/build-a-blog-TP'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
@@ -22,19 +22,20 @@ class Blog(db.Model):
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    return redirect('blog.html')
+    return redirect("/blog")
 
 @app.route("/blog")
 def blog():
     blogs = Blog.query.all()
-    return render_template('blog.html')
+    return render_template('blog.html', blogs=blogs)
 
 @app.route("/blog-entry")
 def blog_entry():
-    entry = Blog.query.get(id)
     id = int(request.args.get('id'))
+    entry = Blog.query.get(id)
+
     return render_template('blog-entry.html', blog=entry )
-    
+
 
 
 
@@ -47,7 +48,7 @@ def newpost():
         blog_title = request.form['blog-title']
         blog_body = request.form['blog-body']
 
-        if blog_title == '': #Return and error message if title is blank 
+        if blog_title == '': #Return and error message if title is blank
             title_error = "Please fill in the title"
         if blog_body == '': #Return an error message if body is blank
             body_error = "Please fill in the body"
@@ -60,8 +61,8 @@ def newpost():
             db.session.add(new_post)
             db.session.commit()
 
-        blogs = Blog.query.all()
-        return render_template("blog-entry.html",title = "Blogs in Space", blog = blogs) 
+        #blogs = Blog.query.get()
+        return render_template("blog-entry.html",title = "Blogs in Space", blog = new_post)
     return render_template('newpost.html')
 
 
